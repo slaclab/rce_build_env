0468f35ed4ad97f3cd8e799d84788ea000cbc8d2
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2022-06-20 18:40:18 -0700

    add function to build device drivers

--- scripts/build_drivers.sh
+++ scripts/build_drivers.sh
@@ -1,55 +1,64 @@
 #!/bin/bash
 _root=$(dirname ${BASH_SOURCE})
-_root=$(cd ${_root}/> /dev/null 2>&1 && pwd)
+_root=$(cd ${_root}/../> /dev/null 2>&1 && pwd)
 
-source ${_root}/../configs/kernel.conf
+function build_drivers {
+  GIT_TAG=$1
+  kernel_tag=$2
+  kernel_ver=$3
+  arch=$4
+  GIT_REPO=https://github.com/slaclab/aes-stream-drivers
+  if [ "${arch}" == "arm64" ]
+  then
+    comp="aarch64-linux-gnu-"
+    kernel_arch="arm64"
+  elif [ "${arch}" == "armhf" ]
+  then
+    comp="arm-linux-gnueabihf-"
+    kernel_arch=arm
+  else
+    echo "Unknown CPU arch ${arch}" && exit 1
+  fi
 
-GIT_REPO=https://github.com/slaclab/aes-stream-drivers
-GIT_TAG=5.17.2
+  cd ${_root}
+
+  _topdir=${_root}/build/drivers/${GIT_TAG}
 
 
-_topdir=${_root}/../build_drivers
+  pkg=$(ls ./build/kernel/linux-headers-${kernel_tag}-${arch}*.deb)
+  echo Installing kernel header package ${pkg}
 
-mkdir -p ${_topdir}/arm64/buildroot/lib/modules/${kernel_ver}-arm64/
+  sudo apt-get -y install ${pkg} || echo "Failed to install kernel headers"
 
-cd ${_topdir}/arm64/
-git clone -b ${GIT_TAG} ${GIT_REPO}
+  mkdir -p ${_topdir}/${arch}/buildroot/lib/modules/${kernel_tag}-${arch}/
 
-for driver in rce_memmap rce_stream
-do
-  echo $driver
-  cd ${_topdir}/arm64/aes-stream-drivers/${driver}/driver
-  make KDIR=/usr/src/linux-headers-${kernel_ver}-arm64 ARCH=arm64 COMP=aarch64-linux-gnu-
-done
-cp $(find ${_topdir}/arm64/aes-stream-drivers/ -name "*.ko") ${_topdir}/arm64/buildroot/lib/modules/${kernel_ver}-arm64/
-fpm -C ${_topdir}/arm64/buildroot --prefix /  \
+  cd ${_topdir}/${arch}
+  git clone -b ${GIT_TAG} ${GIT_REPO}
+
+  for driver in rce_memmap rce_stream
+  do
+    echo $driver
+    cd ${_topdir}/${arch}/aes-stream-drivers/${driver}/driver
+    make KDIR=/usr/src/linux-headers-${kernel_tag}-${arch} ARCH=${kernel_arch} COMP=${comp}
+  done
+  GIT_TAG=$(echo ${GIT_TAG} | sed 's/v//g')
+  cp $(find ${_topdir}/${arch}/aes-stream-drivers/ -name "*.ko") ${_topdir}/${arch}/buildroot/lib/modules/${kernel_tag}-${arch}/
+  fpm -C ${_topdir}/${arch}/buildroot --prefix /  \
 	-s dir -t deb  --name rce_drivers \
-	--license agpl3 --version 15.3.0-5.15 \
-	--architecture arm64 \
-	--depends linux-image-5.15.0-slac-arm64 \
+	--license agpl3 --version ${GIT_TAG}-${kernel_ver} \
+	--architecture ${arch} \
+	--depends linux-image-${kernel_tag}-${arch} \
 	--description "SLAC RCE kernel drivers" \
 	--maintainer "Matthias Wittgen (wittgen@slac.stanford.edu" \
 	.=/
 
-mkdir -p ${_topdir}/arm/buildroot/lib/modules/${kernel_ver}-armhf/
-cd ${_topdir}/arm/
-git clone -b ${GIT_TAG} ${GIT_REPO}
+}
 
-for driver in rce_memmap rce_stream
-do
-  cd ${_topdir}/arm/aes-stream-drivers/${driver}/driver
-  make KDIR=/usr/src/linux-headers-${kernel_ver}-armhf ARCH=arm COMP=arm-linux-gnueabihf-
-done
-cp $(find ${_topdir}/arm/aes-stream-drivers/ -name "*.ko") ${_topdir}/arm/buildroot/lib/modules/${kernel_ver}-armhf/
-fpm -C ${_topdir}/arm/buildroot --prefix /  \
-        -s dir -t deb  --name rce_drivers \
-        --license agpl3 --version 15.3.0-5.15 \
-        --architecture armhf \
-        --depends linux-image-5.15.0-slac-armhf \
-        --description "SLAC RCE kernel drivers" \
-        --maintainer "Matthias Wittgen (wittgen@slac.stanford.edu" \
-        .=/
+build_drivers "5.17.2" "5.15.0-slac" "5.15.0" "arm64"
+build_drivers "5.17.2" "5.15.0-slac" "5.15.0" "armhf"
 
+build_drivers "v5.15.0" "4.14.0-slac" "4.14.0" "arm64"
+build_drivers "v5.4.0" "4.14.0-slac" "4.14.0" "armhf"
 
 
 
--- scripts/build_kernel.sh
+++ scripts/build_kernel.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 _root=$(dirname ${BASH_SOURCE})
-_root=$(cd ${_root}/> /dev/null 2>&1 && pwd)
-
-source ${_root}/../configs/kernel.conf
-
-mkdir -p build/kernel
-cd build/kernel
-git clone --depth 1 https://github.com/mwittgen/linux-xlnx-slac -b ${tag} ${tag}
-cd ${tag}
+_root=$(cd ${_root}/../> /dev/null 2>&1 && pwd)
 
 
-echo Building kernel version ${kernel_ver}, tag ${tag}
-make distclean
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- rcezcu102_defconfig
-sed -i "s|CONFIG_LOCALVERSION_AUTO=.*|CONFIG_LOCALVERSION_AUTO=n|" .config
-make ARCH=arm64 LOCALVERSION=-slac-arm64 CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg -j$(nproc)
+function build_kernel() {
+  tag=$1
+  kernel_ver=$2
+  mkdir -p ${_root}/build/kernel/
+  cd ${_root}/build/kernel/
+  git clone --depth 1 https://github.com/mwittgen/linux-xlnx-slac -b ${tag} ${tag}
+  cd ${tag}
+  echo Building kernel version ${kernel_ver}, tag ${tag}
+  make distclean
+  make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- rcezcu102_defconfig
+  sed -i "s|CONFIG_LOCALVERSION_AUTO=.*|CONFIG_LOCALVERSION_AUTO=n|" .config
+  make ARCH=arm64 LOCALVERSION=-slac-arm64 CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg -j$(nproc)
 
-make distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zynqrce_defconfig
-sed -i "s|CONFIG_LOCALVERSION_AUTO=.*|CONFIG_LOCALVERSION_AUTO=n|" .config
-make ARCH=arm LOCALVERSION=-slac-armhf CROSS_COMPILE=arm-linux-gnueabihf- bindeb-pkg -j$(nproc)
+  make distclean
+  make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zynqrce_defconfig
+  sed -i "s|CONFIG_LOCALVERSION_AUTO=.*|CONFIG_LOCALVERSION_AUTO=n|" .config
+  make ARCH=arm LOCALVERSION=-slac-armhf CROSS_COMPILE=arm-linux-gnueabihf- bindeb-pkg -j$(nproc)
+}
 
-sudo apt-get -y install ./build/kernel/linux-headers-*.deb
+build_kernel "slac-v2022.1" "5.15.0-slac"
+build_kernel "slac-v2018.3" "4.14.0-slac"
+
